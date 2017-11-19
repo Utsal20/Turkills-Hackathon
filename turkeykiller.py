706bf962896e256313cb827ad93825458d4c1d90
Amon <amonsthapit@live.com>
2017-11-18 18:27:36 -0800

    main game update

--- turkeykiller.py
+++ turkeykiller.py
@@ -7,6 +7,8 @@ Created on Sat Nov 18 11:38:14 2017
 
 import pygame
 import os
+import random
+import time
 
 #image library function
 _image_library = {}
@@ -29,7 +31,7 @@ bg = get_image('landscape.jpg')
 #score text
 pygame.font.init()
 myfont = pygame.font.SysFont('Arial', 30)
-score = int(1);
+score = int(0);
 textsurface = myfont.render('Score: '+str(score), False, (0, 0, 0))
 def update_score(i):
         global textsurface
@@ -39,13 +41,34 @@ def update_score(i):
         screen.blit(textsurface,(0,0))
 update_score(0)
 
-#turkey image
-turkey_list = [(50,50),(500,500),(150,500)]
-turkey = pygame.transform.scale(get_image('turkey.png'),(150,150))
+#turkey images
+turkey = pygame.transform.scale(get_image('turkey.png'),(100,100))
+turkey_list = (500,500)
 def show_turkeys():
-        for i in turkey_list:
-                screen.blit(turkey, i)
+        screen.blit(turkey,turkey_list)
+        #for i in turkey_list:
+                #screen.blit(turkey, i)
 
+def new_turkey():
+        return int(random.randint(0,1000)/100)*100,int(random.randint(0,800)/100)*100
+
+def turkey_killed_or_not(mouse_pos):
+        global turkey_list
+        global time1
+        pos=(0,0)
+        pos = int((mouse_pos[0])/100)*100,int((mouse_pos[1])/100)*100
+        print('pos: ',pos)
+        if pos == turkey_list:
+                update_score(200)
+                turkey_list = new_turkey()
+                time1 = time.time()
+            
+        
+mouse_pos = (0,0)
+time1 = time.time()
+time_level = 2
+
+timer = time.time()
 
 #gameloop
 done = False
@@ -54,9 +77,19 @@ while not done:
                 if event.type == pygame.QUIT:
                         done = True
                 if event.type == pygame.MOUSEBUTTONDOWN:
-                        if event.button == 1: # left click grows radius 
-                                update_score(5)
-                
+                        if event.button == 1:
+                                mouse_pos = pygame.mouse.get_pos()
+                                print(mouse_pos)
+                                turkey_killed_or_not(mouse_pos)
+                                
+        if time.time()-time1 > time_level:
+                turkey_list = new_turkey()
+                time1 = time.time()
+        
+        print("Time: ", 30-(time.time()-timer) )        
+        if time.time()-timer>30:
+                done = True
+                                        
         screen.blit(bg, (0,0))
         update_score(0)
         show_turkeys()
@@ -64,3 +97,8 @@ while not done:
         pygame.display.flip()
         #os.clock.tick(60)
         
+#save score to file
+def save_score(score):
+        with open('highscores.txt','a') as filehandler:
+                filehandler.writelines('name'+' - '+str(score)+'\n')
+save_score(score)
