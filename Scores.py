a3bca5abbc0348ac388f298c5108116c584da1b1
anujinm <anujin.munhbat@gmail.com>
2017-11-19 18:09:17 -0800

    Add files via upload

--- Scores.py
+++ Scores.py
@@ -5,10 +5,10 @@ from Turckeys import text_objects
 from Turckeys import get_image
 from operator import itemgetter
 
-white = (245,245,220)
-wheat = (245, 222,179)
-black = ((0,0,0))
-gold = (218,165,32)
+white = (245, 245, 220)
+wheat = (245, 222, 179)
+black = ((0, 0, 0))
+gold = (218, 165, 32)
 
 # initialize screen
 pygame.init()
@@ -16,43 +16,45 @@ screen = pygame.display.set_mode((1000, 800))
 pygame.display.set_caption("Guru's Turkeys")
 done = False
 
-def scorestitle(x,y,w,h):
+
+def scorestitle(x, y, w, h):
     smallText = pygame.font.Font("freesansbold.ttf", 50)
     textSurf, textRect = text_objects("High Scores:", smallText)
-    textRect.center = ((x + (w / 2)), (y + (h/ 2)))
+    textRect.center = ((x + (w / 2)), (y + (h / 2)))
     screen.blit(textSurf, textRect)
 
-def lines(num,x,y,w,h):
-    for line in range(1,10):
+
+def lines(num, x, y, w, h):
+    for line in range(1, 10):
         smallText = pygame.font.Font("freesansbold.ttf", 30)
         textSurf, textRect = text_objects(str(num) + ". ", smallText)
         textRect.center = ((x + (w / 2)), (y + (h / 2)))
         screen.blit(textSurf, textRect)
+
+
 def getScores():
-    x=[]
+    x = []
     with open('highscores.txt', 'r') as fileee:
         d = fileee.readlines()
         for l in d:
             n = l.strip('\n').split(' ')
-            #print(n)
+            # print(n)
             x.append(n)
-        #x.remove([''])
-    x = sorted(x, key = itemgetter(1), reverse=True)
+        # x.remove([''])
+        print(x)
+    x = sorted(x, key=itemgetter(1), reverse=True)
     return x
-def showScores(score,x,y,w,h):
-    for line in range(1,10):
+
+
+def showScores(score, x, y, w, h):
+    for line in range(1, 10):
         smallText = pygame.font.Font("freesansbold.ttf", 30)
         textSurf, textRect = text_objects(score, smallText)
         textRect = ((x + (w / 2)), (y + (h / 2)))
         screen.blit(textSurf, textRect)
-        
-#turkey images
-turkey = pygame.transform.scale(get_image('cookedTurkey.png'), (350, 200))
-runningTurkey = pygame.transform.scale(get_image('runningTurkey.png'), (190, 130))
 
-        
-scores = getScores()
-print(scores)
+
+
 
 def scorescreen():
     global done
@@ -61,44 +63,34 @@ def scorescreen():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 done = True
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if event.button == 1:
-                    mouse_pos = pygame.mouse.get_pos()
-                    print(mouse_pos)
-                    if 350<mouse_pos[0]<560 and 700<mouse_pos[1]<760:
-                        print('play again button clicked')
-                        done = True
-                        return 7
-                    elif 610<mouse_pos[0]<760 and 700<mouse_pos[1]<760:
-                        print('exit button clicked')
-                        done = True
-                        return 2
-    
+
         screen.fill((173, 216, 230))
-        scorestitle(150,50,100,40)
-    
-        #lines
+        scorestitle(150, 50, 100, 40)
+
+        # lines
         h = 150
-        for i in range(1,11):
-            lines(i,30,h,40,40)
+        for i in range(1, 11):
+            lines(i, 30, h, 40, 40)
             h += 50
-        #scores
+            # scores
             h2 = 135
-            for i in range(10):
-                score = scores[i][0] + " " + scores[i][1]
-                showScores(score,30,h2,100,40)
-                h2 += 50
-    
-        button("Play Again!", 350,700,210,60,white,gold,30)
-        button("Exit", 610,700,150,60,white, gold,30)
-    
-    
+        scores = getScores()
+        for i in range(10):
+            score = scores[i][0] + " " + scores[i][1]
+            showScores(score, 30, h2, 100, 40)
+            h2 += 50
+
+        button("Play Again!", 350, 700, 210, 60, white, gold, 30)
+        button("Exit", 610, 700, 150, 60, white, gold, 30)
+
+        turkey = pygame.transform.scale(get_image('cookedTurkey.png'), (350, 200))
         screen.blit(turkey, (630, 20))
+        runningTurkey = pygame.transform.scale(get_image('runningTurkey.png'), (190, 130))
         screen.blit(runningTurkey, (100, 670))
-    
+
         pygame.display.flip()
         # os.clock.tick(60)
-        
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     scorescreen()
--- turkeykiller.py
+++ turkeykiller.py
@@ -1,130 +1,136 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Nov 18 11:38:14 2017
-
-@author: Amon, Utsal, Anujin, Ene
-"""
-
 import pygame
 import os
 import random
 import time
 from pygame import mixer
+from Turckeys import startscreen
 
-#initialize screen
+# initialize screen
 pygame.init()
 screen = pygame.display.set_mode((1000, 800))
 
-#image library function
+# image library function
 _image_library = {}
+
+
 def get_image(path):
-        global _image_library
-        image = _image_library.get(path)
-        if image == None:
-                canonicalized_path = path.replace('/', os.sep).replace('\\', os.sep)
-                image = pygame.image.load(canonicalized_path)
-                _image_library[path] = image
-        return image
-    
-#background color
-screen.fill((175,238,238))
+    global _image_library
+    image = _image_library.get(path)
+    if image == None:
+        canonicalized_path = path.replace('/', os.sep).replace('\\', os.sep)
+        image = pygame.image.load(canonicalized_path)
+        _image_library[path] = image
+    return image
+
+
+# background color
+screen.fill((175, 238, 238))
 bg = get_image('landscape.jpg')
 
-#score text
+# score text
 pygame.font.init()
 myfont = pygame.font.SysFont('Arial', 30)
 score = int(0);
-textsurface = myfont.render('Score: '+str(score), False, (0, 0, 0))
+textsurface = myfont.render('Score: ' + str(score), False, (0, 0, 0))
+
+
 def update_score(i):
-        global textsurface
-        global score
-        score = score + i
-        textsurface = myfont.render('Score: '+str(score), False, (0, 0, 0))
-        screen.blit(textsurface,(0,0))
+    global textsurface
+    global score
+    score = score + i
+    textsurface = myfont.render('Score: ' + str(score), False, (0, 0, 0))
+    screen.blit(textsurface, (0, 0))
+
+
 update_score(0)
 
-#turkey images
-turkey = pygame.transform.scale(get_image('turkey.png'),(100,100))
-turkey_list = (500,500)
+# turkey images
+turkey = pygame.transform.scale(get_image('turkey.png'), (100, 100))
+turkey_list = (500, 500)
+
+
 def show_turkeys():
-        screen.blit(turkey,turkey_list)
-        #for i in turkey_list:
-                #screen.blit(turkey, i)
-                
-#turkey sounds
+    screen.blit(turkey, turkey_list)
+    # for i in turkey_list:
+    # screen.blit(turkey, i)
+
+
+# turkey sounds
 mixer.init()
 mixer.music.load('turkeysound2.wav')
 
+
 def new_turkey():
-        return int(random.randint(0,1000)/100)*100,int(random.randint(0,800)/100)*100
+    return int(random.randint(0, 1000) / 100) * 100, int(random.randint(0, 800) / 100) * 100
 
-#check if turkey was brutally murdered and do something if it was...
+
+# check if turkey was brutally murdered and do something if it was...
 def turkey_killed_or_not(mouse_pos):
-        global turkey_list
-        global time1
-        global time_level
-        pos=(0,0)
-        pos = int((mouse_pos[0])/100)*100,int((mouse_pos[1])/100)*100
-        print('pos: ',pos)
-        if pos == turkey_list:
-                update_score(200)
-                if mixer.music.get_busy():
-                        mixer.music.stop()
-                        print('sound stoppped')
-                mixer.music.play()
-                time_level = time_level * 0.9;
-                print('timelevel: ',time_level)
-                print('turkey killed')
-                print('sound played')
-                turkey_list = new_turkey()
-                time1 = time.time()
-            
-#save score to file
-def save_score(score):
-        with open('highscores.txt','a') as filehandler:
-                filehandler.writelines('name '+str(score)+'\n')
-        print('highscore saved: ','name ',score)
-        
-mouse_pos = (0,0)
+    global turkey_list
+    global time1
+    global time_level
+    pos = (0, 0)
+    pos = int((mouse_pos[0]) / 100) * 100, int((mouse_pos[1]) / 100) * 100
+    print('pos: ', pos)
+    if pos == turkey_list:
+        update_score(200)
+        if mixer.music.get_busy():
+            mixer.music.stop()
+            print('sound stoppped')
+        mixer.music.play()
+        time_level = time_level * 0.9;
+        print('timelevel: ', time_level)
+        print('turkey killed')
+        print('sound played')
+        turkey_list = new_turkey()
+        time1 = time.time()
 
+name = startscreen()
+# save score to file
+def save_score(score):
+    with open('highscores.txt', 'a') as filehandler:
+        filehandler.writelines(name + ' ' + str(score) + '\n')
+    print('highscore saved: ', name, score)
+
+
+mouse_pos = (0, 0)
+time1 = time.time()
 time_level = 2.0
 
+timer = time.time()
+
+
 def playgame():
-        global time1, turkey_list
-        #gameloop
-        done = False
-        time1 = time.time()
-        timer = time.time()
+    global time1, turkey_list
+    # gameloop
+    done = False
+    while not done:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    mouse_pos = pygame.mouse.get_pos()
+                    print(mouse_pos)
+                    turkey_killed_or_not(mouse_pos)
 
-        while not done:
-                for event in pygame.event.get():
-                        if event.type == pygame.QUIT:
-                                done = True
-                        if event.type == pygame.MOUSEBUTTONDOWN:
-                                if event.button == 1:
-                                        mouse_pos = pygame.mouse.get_pos()
-                                        print(mouse_pos)
-                                        turkey_killed_or_not(mouse_pos)
-                                        
-                if time.time()-time1 > time_level:
-                        turkey_list = new_turkey()
-                        time1 = time.time()
-                
-                print("Time: ", 30-(time.time()-timer) )        
-                if time.time()-timer>30:
-                        done = True
-                
-                                
-                screen.blit(bg, (0,0))
-                update_score(0)
-                show_turkeys()
-                
-                pygame.display.flip()
-                #os.clock.tick(60)
-                
-        save_score(score)
-        
+        if time.time() - time1 > time_level:
+            turkey_list = new_turkey()
+            time1 = time.time()
 
-if __name__=='__main__':
-        playgame()
+        print("Time: ", 30 - (time.time() - timer))
+        if time.time() - timer > 30:
+            done = True
 
+        screen.blit(bg, (0, 0))
+        update_score(0)
+        show_turkeys()
+
+        pygame.display.flip()
+        # os.clock.tick(60)
+
+    save_score(score)
+
+
+if __name__ == '__main__':
+    playgame()
